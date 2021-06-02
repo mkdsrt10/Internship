4fe1396d3f239ce9f11e9ccb65605383a80a8daa
Mayank <dmayank0@gmail.com>
2021-06-02 20:19:07 +0530

    A

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3005/",
+        "redirectSignOut": "http://localhost:3005/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS"
